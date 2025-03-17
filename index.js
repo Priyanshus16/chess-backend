2961d189eb7b187ac89eedeb6cd08710af4fe612
Palash <palash.shah@metafic.co>
2025-03-17 22:52:51 +0530

    add .gitignore, update dev script to use nodemon, and improve error response for user login

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user) return res.status(200).send("User not found");
+    if (!user) return res.status(400).json({message:"User not found"});
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).send("Invalid credentials");
